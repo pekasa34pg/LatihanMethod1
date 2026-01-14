49b9d84b56481641d5911259e21f9a6e3e3f04ac
perkasa tricahya putra dewa patrikha <perkaspatrikha123@gmail.com>
2026-01-14 08:51:24 +0700

    membuat method tambah ,kurang ,kali,bagi

--- LatihanMethod1/Program.cs
+++ LatihanMethod1/Program.cs
@@ -1,7 +1,47 @@
-﻿static void tambah ()  
+﻿static void tambah ()
 {
     int a = 10, b = 2, c = 0;
-  
+
     c = a + b;
-    Console.WriteLine($"{a} + {b} = {c}");
+  Console.WriteLine($"{a} + {b} = {c}");
 }
+
+
+
+
+static void kurang ()
+{
+    int a = 10, b = 2, c = 0;
+
+    c = a - b;
+    Console.WriteLine($"{a} - {b} = {c}");
+}
+
+
+
+
+
+
+static void kali ()
+{
+    int a = 10, b = 2, c = 0;
+
+    c = a * b;
+    Console.WriteLine($"{a} * {b} = {c}");
+}
+
+
+
+
+static void bagi ()
+{
+    int a = 10, b = 2, c = 0;
+
+    c = a / b;
+    Console.WriteLine($"{a} / {b} = {c}");
+}
+
+tambah(); 
+kurang(); 
+kali();
+bagi();
